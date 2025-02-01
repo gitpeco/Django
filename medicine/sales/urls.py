c50d1d749bcf3f032c9c84d262daccad6984e649
gitpeco <znm9110@163.com>
2025-02-01 23:54:55 +0800

    修改urls中的内容

--- medicine/sales/urls.py
+++ medicine/sales/urls.py
@@ -3,8 +3,8 @@ from sales.views import listorders,listorders1,listorders2,listorders3
 
 urlpatterns = [
 
-    path('orders',listorders,name='listorders'),
-    path('orders1',listorders1,name='listorders1'),
-    path('orders2',listorders2,name='listorders2'),
-    path('orders3',listorders3,name='listorders3'),
+    path('orders/',listorders,name='listorders'),
+    path('orders1/',listorders1,name='listorders1'),
+    path('orders2/',listorders2,name='listorders2'),
+    path('orders3/',listorders3,name='listorders3'),
 ]
